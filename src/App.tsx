1c237b7560abff979c102be5706733636a3e64db
Philip Minnebach <philip@minnebach.be>
2025-03-31 15:07:30 +0200

    add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import Header from './components/header'
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header title='TimeTracking App' />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count may be {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
