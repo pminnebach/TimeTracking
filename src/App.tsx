e0ee42ada5d00ec9e5b679925c16d15fca3b925b
Philip Minnebach <philip@minnebach.be>
2025-04-01 14:40:37 +0200

    Update configuration for size optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import ButtonGroup from "./components/ButtonGroup";
 import Header from "./components/header";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<Header title="TimeTracking App" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		visualizer({
+			open: true,
+			gzipSize: true,
+			brotliSize: true,
+		}),
+	],
 	build: {
 		// generate .vite/manifest.json in outDir
 		manifest: true,
+		minify: "terser",
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ["react", "react-dom"],
+				},
+			},
+		},
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
 	},
 });
